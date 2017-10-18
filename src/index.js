8d96b8230139a54d3be1a68371e18d23bb29c635
Eliathah Boda <nerboda@gmail.com>
2017-10-18 17:55:44 -0600

    Delete unecessary stuff in README

--- src/index.js
+++ src/index.js
@@ -15,40 +15,6 @@ import './assets/style.css';
 
 module.hot.accept();
 
-/* 
-
-Outline: a basic app that shows weather information for current location and allows you to add other locations
-
-Details:
-  - load current location and pass coordinates to Location component
-    - use navigator geolocation to get current coordinates
-    - api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={key}
-    - api key -> 1f874c21b9ca244ffdc7f0573bfe1b7c
-  - have an AddLocation component which requires you to manually enter location
-    - state of the app is stored in redux store
-    - dispatch an addLocation action
-    - pass location props to location component
-    - location component is responsible for fetching weather data
-      - fetch weather data using city name
-      - api.openweathermap.org/data/2.5/weather?q={city name}&appid={key}
-      - api key -> 1f874c21b9ca244ffdc7f0573bfe1b7c
-  - you can also remove locations
-    - dispatch remove location action
-    - pass index of location
-
-Flow:
-  - you load page, it says "Determining your location..."
-  - once you allow geolocator, it renders the app with your location
-  - once the component for your location is rendered, it gets weather info
-  - once that weather info is received, component is updated
-  - you can then add/remove locations
-    - if you add a location, the component is rendered to the dom
-      - weather info is then fetched for that location
-      - once the data comes back, the comonent is updated
-    - if you remove a location, an action is dispatched to update the store minus that location
-
-*/
-
 class App extends Component {
   constructor(props) {
     super(props);
